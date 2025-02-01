c597324c81d67f3df2af97a6c53c756c5d9e5270
Joost Yervante Damad <joost@damad.be>
2025-02-01 11:34:33 +0100

    update pages; fixed update of individual screenshot pages

--- update.sh
+++ update.sh
@@ -10,9 +10,18 @@ cd tmp
 # main website (requires PHP 7+ to build)
 git clone --depth 1 https://github.com/openMSX/htdocs.git
 make -C htdocs
+cp -av htdocs/* ..
 
 # openMSX documentation
 git clone --depth 1 https://github.com/openMSX/openMSX.git
+cp -av openMSX/doc/manual/* ../manual
+cp -av openMSX/doc/internal/vdp-vram-timing/* ../vdp-vram-timing
 
 # catapult documentation
 git clone --depth 1 https://github.com/openMSX/wxcatapult.git
+cp -av wxcatapult/doc/manual/* ../catapult-manual
+
+cd ..
+
+echo
+echo "Review the changes, and when ready, commit and push and the website will update."
